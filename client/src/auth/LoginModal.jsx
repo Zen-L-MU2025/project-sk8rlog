bea8ddf907a2c21ea3386a54a77c09af34122fa3
Zen Lambertus <zenl@meta.com>
2025-06-24 19:39:22 -0700

    Reroute on login, cont; can move onto authorization flow now

--- client/src/auth/LoginModal.jsx
+++ client/src/auth/LoginModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router';
 
 import * as user from '../utils/userUtils'
@@ -9,14 +9,21 @@ const LoginModal = () => {
     const navigate = useNavigate();
     const [isSuccessful, setIsSuccessful] = useState(null);
 
+    useEffect(() => {
+        isSuccessful && navigate('/home')
+    }, [isSuccessful])
+
+    const handleForm = async (formData) => {
+        user.handleLoginRegister(formData, "login", setIsSuccessful)
+    }
+
     return (
-        <form className='landingModal' action={(formData) => user.handleLoginRegister(formData, "login", setIsSuccessful)}>
+        <form className='landingModal' action={handleForm}>
             <h1>Welcome back!</h1>
             <input type='text' name='username' placeholder='Username' required />
             <input type='password' name='password' placeholder='Password' required />
             <button type='submit' onSubmit={(event) => event.preventDefault()}>Log In</button>
             <Link to='/register'> I want to create a new account </Link>
-            {isSuccessful && navigate('/home')}
             {isSuccessful === false && <p>Invalid username or password</p>}
         </form>
     )
