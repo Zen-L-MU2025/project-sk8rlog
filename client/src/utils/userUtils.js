cf42c62723d3f783a09d7e478a1d03470930b9a7
Zen Lambertus <zenl@meta.com>
2025-06-26 12:09:16 -0700

    Overhauled handleLogin(or)Register; Implemented authentication functionality using sessionStorage; created verifyAccess helper

--- client/src/utils/userUtils.js
+++ client/src/utils/userUtils.js
@@ -2,7 +2,6 @@ const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 import axios from 'axios'
 
-
 // Tracks if landing page submission is login or register
 export const OPTIONS = {
     LOGIN : "login",
@@ -10,37 +9,76 @@ export const OPTIONS = {
 }
 
 // Handle login/register submission
-export const handleLoginRegister = (formData, submissionType, setIsSuccessful) => {
+export const handleLoginOrRegister = (formData, submissionType, setIsSuccessful) => {
     const username = formData.get("username"), password = formData.get("password")
 
-    submissionType == OPTIONS.LOGIN      && login(username, password, setIsSuccessful)
-    submissionType == OPTIONS.REGISTER   && register(username, password, setIsSuccessful)
+    switch (submissionType) {
+        case OPTIONS.LOGIN:
+            login(username, password, setIsSuccessful)
+            break
 
-    if (submissionType != OPTIONS.LOGIN && submissionType != OPTIONS.REGISTER) {
-        console.error("Invalid type for handleLoginRegister")
+        case OPTIONS.REGISTER:
+            register(username, password, setIsSuccessful)
+            break
+
+        default:
+            console.error("Invalid type for handleLoginRegister")
     }
 }
 
+// Handle login: store token and user in session storage on completion
 export const login = (username, password, setIsSuccessful) => {
     axios.post(`${baseUrl}/users/login`, {username, password})
         .then(res => {
-            console.log("Login successful!")
             setIsSuccessful(res.data.isSuccessful)
+            sessionStorage.setItem("webtoken", res.data.token)
+            sessionStorage.setItem("user", res.data.user)
         })
-        .catch(err => {
-            console.log("Login failed!")
+        .catch(error => {
             setIsSuccessful(false)
         })
 }
 
+// Handle registration: store token and new user in session storage on completion
 export const register = (username, password, setIsSuccessful) => {
     axios.post(`${baseUrl}/users/register`, {username, password})
         .then(res => {
-            console.log("Registration successful!")
             setIsSuccessful(res.data.isSuccessful)
+            sessionStorage.setItem("webtoken", res.data.token)
+
+            const newUser = res.data.newUser
+            // No need to store password in session
+            newUser.remove("password")
+            sessionStorage.setItem("user", newUser)
         })
-        .catch(err => {
-            console.log("Registration failed!")
+        .catch(error => {
             setIsSuccessful(false)
         })
+}
+
+// Verify user access to protected resource
+// Sets hasAccess to true if access is verified, false otherwise
+export const verifyAccess = (setHasAccess) => {
+
+    const token = sessionStorage.getItem("webtoken")
+
+    if (!token) {
+        setHasAccess(false)
+    }
+
+    axios.get(`${baseUrl}/auth/verify`, {headers : { 'Authorization' : `Bearer ${token}` }})
+        .then(res => {
+            const isSuccessful = res.data.isSuccessful
+            if (isSuccessful) {
+                setHasAccess(true)
+            }
+            else {
+                setHasAccess(false)
+            }
+        })
+        .catch(error => {
+            setHasAccess(false)
+        })
+
+
 }
