b33cfa7af96d59294756138300e750ff1d4d3eaa
Zen Lambertus <zenl@meta.com>
2025-06-26 22:52:26 -0700

    actually added TODO statement, whoops

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,6 +8,7 @@ const STATUS_CODES = require('../statusCodes')
 router.use('/verify', async (req, res) => {
     try {
         // Extract the JWT token from the request header
+        // TODO Find better way to accomplish this
         const token = req.headers.authorization.split(' ')[1]
 
         // Verify the token
