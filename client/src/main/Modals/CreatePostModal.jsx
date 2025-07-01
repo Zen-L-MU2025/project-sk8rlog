6a6888a67cb8d8e07241c7d5d4fc6efc8b3ffa90
Zen Lambertus <zenl@meta.com>
2025-06-30 17:29:24 -0700

    Implemented a post creation modal

---
+++ client/src/main/Modals/CreatePostModal.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import { CLIPS, BLOGS } from '/src/utils/constants'
+
+import '/src/css/createPostModal.css'
+
+const CreatePostModal = ({ toggleCreatePostModal }) => {
+    const handleForm = (formData) => {
+
+
+        toggleCreatePostModal()
+    }
+
+    const DEFAULT = 'default'
+    const [postType, setPostType] = useState(DEFAULT)
+
+    const handleSelect = (event) => {
+        setPostType(event.target.value)
+    }
+
+    return (
+        <section id='createPostModal' onClick={toggleCreatePostModal}>
+            <form id='createModalContent' onClick={(event) => event.stopPropagation()} action={handleForm}>
+                <h2>New Post!</h2>
+                <select id='postType' onChange={handleSelect}>
+                    <option value={DEFAULT}>Select Post Type</option>
+                    <option value={CLIPS}>Clip</option>
+                    <option value={BLOGS}>Blog</option>
+                </select>
+
+                { postType === CLIPS && <>
+                    <input type='file' id='clip' name='clip' accept='video/*' />
+                    <input type='text' name='description' placeholder='Description (50 character maximum)' maxLength={50} required />
+
+                </>}
+
+                { postType === BLOGS && <>
+                    <input type='text' name='Title' placeholder='Title (50 character maximum)' maxLength={50} required />
+                    <textarea type='long_text' name='Content' placeholder='Description (300 character maximum)' maxLength={300} required />
+                </>}
+
+
+                { postType !== DEFAULT &&
+                    <button type='submit' onSubmit={(event) => event.preventDefault()}>Upload</button>
+                }
+            </form>
+        </section>
+    )
+}
+
+export default CreatePostModal
