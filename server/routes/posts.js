34e0de6e9e56395e433c23423fe2cdfe04601881
Zen Lambertus <zenl@meta.com>
2025-07-02 00:38:43 -0700

    Implemented posts router; GCS file uploads are now fully operational

---
+++ server/routes/posts.js
@@ -0,0 +1,56 @@
+const { PrismaClient } = require('../generated/prisma');
+const router = require('express').Router()
+const STATUS_CODES = require('../statusCodes')
+const { QUICKTIME, MOV } = require('../constants')
+const GCS = require('../utils/GCS')
+
+const Multer = require('multer')
+const multer = Multer({
+    storage: Multer.memoryStorage()
+})
+
+const prisma = new PrismaClient()
+
+// POST /posts/uploadFile
+// Uploads a pending post's file to GCS and returns the URL
+router.use('/uploadFile', multer.single('postFile'), async (req, res, next) => {
+    try {
+        if (!req.file) {
+            return res.status(STATUS_CODES.BAD_REQUEST).json({ message: 'No file provided', isSuccessful: false })
+        }
+
+        let extension = await new String(req.file.mimetype).split('/')[1]
+        if (extension === QUICKTIME) {
+            extension = MOV
+        }
+        const DESTINATION = `${crypto.randomUUID()}.${extension}`
+        const objectURL = await GCS.uploadFile(req.file, DESTINATION)
+
+        return res.status(STATUS_CODES.CREATED).json({ fileURL : objectURL, message: 'File uploaded' })
+
+    } catch (error) {
+        return res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message, isSuccessful: false })
+    }
+})
+
+// POST /posts/:userID
+// Uploads a post to the database and subsequently links it to the user through their ID
+router.use('/:userID', async (req, res, next) => {
+    try {
+        const { userID } = req.params
+        const { textContent, location, postType, fileURL } = req.body
+
+        const post = await prisma.post.create({
+            data: {
+                authorID: userID, description: textContent, location, type: postType, fileURL
+            }
+        })
+
+        return res.status(STATUS_CODES.CREATED).json({ post, message: 'Post created' })
+
+    } catch (error) {
+        return res.status(STATUS_CODES.SERVER_ERROR).json({ message: error.message, isSuccessful: false })
+    }
+})
+
+module.exports = router
