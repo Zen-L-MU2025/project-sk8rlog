11554135ec57a259077d5b93741faeef1121dd00
Zen Lambertus <zenl@meta.com>
2025-07-02 14:33:40 -0700

    posts now display preview video/header image

--- client/src/main/PostCard.jsx
+++ client/src/main/PostCard.jsx
@@ -1,8 +1,20 @@
+import { useRef } from 'react'
+
 import { CLIPS, BLOGS } from '/src/utils/constants'
 
 import '/src/css/postCard.css';
 
 const PostCard = ({ post, postType }) => {
+    const embedRef = useRef(null)
+
+    const onEnter = () => {
+        embedRef.current.play()
+    }
+    const onLeave = () => {
+        embedRef.current.currentTime = 0
+        embedRef.current.pause()
+    }
+
     if (!post) return (
         <article className={`${postType}PostCard`}>
             <p>No post data found! This is a dummy card.</p>
@@ -12,7 +24,9 @@ const PostCard = ({ post, postType }) => {
     return (
         <article className={`${postType}PostCard`}>
             { postType === CLIPS &&
-                <video className={`${postType}PostCardEmbed`} src={post.fileURL} />
+                <video ref={embedRef} className={`${postType}PostCardEmbed`} src={post.fileURL}
+                    onMouseEnter={onEnter} onMouseLeave={onLeave}
+                />
             }
             { postType === BLOGS &&
                 <img className={`${postType}PostCardEmbed`} src={post.fileURL} alt={post.title} />
