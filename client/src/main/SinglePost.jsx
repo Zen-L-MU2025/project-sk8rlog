a75ee5c0e8eb464e4225accd418417b80d78641f
Zen Lambertus <zenl@meta.com>
2025-07-05 15:12:46 -0700

    Add Footer and Header to SinglePost

--- client/src/main/SinglePost.jsx
+++ client/src/main/SinglePost.jsx
@@ -1,11 +1,18 @@
 import { useParams, Link } from 'react-router'
 
+import Header from './Header'
+import Footer from './Footer'
+
 const SinglePost = () => {
     const { postID } = useParams()
 
+    const HEADER_TEXT = 'Sk8rlog'
+
     return (<>
+        <Header HEADER_TEXT={HEADER_TEXT}/>
         <p>Post {postID}</p>
         <Link to='/home'>Go Back</Link>
+        <Footer />
     </>)
 }
 
