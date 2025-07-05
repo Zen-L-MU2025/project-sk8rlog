882dd309a6603b6dc97e9ac4f4c7069e5c34bf31
Zen Lambertus <zenl@meta.com>
2025-07-05 16:00:44 -0700

    all post info displayable

--- client/src/main/SinglePost.jsx
+++ client/src/main/SinglePost.jsx
@@ -1,16 +1,31 @@
+import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router'
 
 import Header from './Header'
 import Footer from './Footer'
 
+import { getPostByID } from '/src/utils/postUtils'
+import { CLIPS, BLOGS } from '/src/utils/constants'
+
+
 const SinglePost = () => {
     const { postID } = useParams()
-
     const HEADER_TEXT = 'Sk8rlog'
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        getPostByID(postID, setPost)
+    })
 
     return (<>
         <Header HEADER_TEXT={HEADER_TEXT}/>
-        <p>Post {postID}</p>
+        { post?.type === CLIPS && <video src={post?.fileURL} controls={true}></video>}
+        { post?.type === BLOGS && <img src={post?.fileURL} /> }
+        <p>{post?.creationDate}</p>
+        <p>{post?.description}</p>
+        <p>{post?.location}</p>
+        <p>{post?.type}</p>
+        <p>{post?.likeCount}</p>
         <Link to='/home'>Go Back</Link>
         <Footer />
     </>)
