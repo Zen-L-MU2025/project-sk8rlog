94fc8533e8846ff19f79bfac6d0e7de43a2de6dd
Zen Lambertus <zenl@meta.com>
2025-07-07 11:59:57 -0700

    handle post deletion

--- client/src/main/PostCard.jsx
+++ client/src/main/PostCard.jsx
@@ -1,11 +1,15 @@
 import { useRef } from 'react'
 import { Link } from 'react-router'
 
+import { deletePost } from '/src/utils/postUtils'
+
 import { CLIPS, BLOGS } from '/src/utils/constants'
+import { PROFILE_ORIGIN } from '/src/utils/constants'
 
 import '/src/css/postCard.css';
+import trash from '/src/assets/trash.png';
 
-const PostCard = ({ post, postType, origin }) => {
+const PostCard = ({ post, postType, origin, setUserPosts }) => {
     const embedRef = useRef(null)
 
     const onEnter = () => {
@@ -14,6 +18,11 @@ const PostCard = ({ post, postType, origin }) => {
     const onLeave = () => {
         embedRef.current.currentTime = 0
         embedRef.current.pause()
+    }
+
+    const handleDeletePost = (event) => {
+        event.preventDefault()
+        deletePost(post, setUserPosts)
     }
 
     if (!post) {
@@ -33,6 +42,11 @@ const PostCard = ({ post, postType, origin }) => {
                 <img className={`${postType}PostCardEmbed`} src={post.fileURL} alt={post.title} />
             }
             <p>{post.description}</p>
+
+            { origin === PROFILE_ORIGIN &&
+                <img className='deletePostButton' src={trash} onClick={handleDeletePost} alt='trash icon' />
+            }
+
         </article>
         </Link>
     )
