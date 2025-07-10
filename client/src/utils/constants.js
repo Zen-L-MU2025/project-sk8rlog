a5f82b696d4f6d38ceee5f32d000081cea0e915a
Zen Lambertus <zenl@meta.com>
2025-07-10 09:50:25 -0700

    date cutoff constants

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -45,3 +45,6 @@ export const LIKE = "like"
 export const UNLIKE = "unlike"
 
 export const NON_ALPHANUMERIC_REGEX = /[^a-zA-Z0-9]/
+
+export const MILLISECONDS_IN_DAY = 86400000
+export const AGE_CUTOFF_IN_DAYS = 7
