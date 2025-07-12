c0a37144e0d1adfb931bfe1e46939a7cddd78f54
Zen Lambertus <zenl@meta.com>
2025-07-11 23:43:49 -0700

    sorting

--- client/src/main/Posts.jsx
+++ client/src/main/Posts.jsx
@@ -7,36 +7,64 @@ import Footer from './Footer'
 import PostCard from './PostCard'
 
 import UserContext from '/src/utils/UserContext'
-import { verifyAccess } from '/src/utils/UserUtils'
+import { verifyAccess, loadUserSession } from '/src/utils/UserUtils'
 import  { getAllPostsByType } from '/src/utils/postUtils'
-import { POSTS } from '/src/utils/constants'
+import { POSTS, RECOMMENDED, LATEST, POPULAR, NEAR_YOU } from '/src/utils/constants'
 
 import '/src/css/hasSidebar.css'
 import '/src/css/posts.css'
 
 const Posts = ({ postType }) => {
-    const { activeUser } = useContext(UserContext)
+    const { activeUser, setActiveUser } = useContext(UserContext)
+    const loadUser = async () => { await loadUserSession(setActiveUser) }
+
+    const [hasAccess, setHasAccess] = useState(null)
+    const [isReadyToDisplayContent, setIsReadyToDisplayContent] = useState(false)
+    const [posts, setPosts] = useState(null)
+    const [filterState, setFilterState] = useState(RECOMMENDED)
 
     const navigate = useNavigate()
-    const [hasAccess, setHasAccess] = useState(null)
+
+    const HEADER_TEXT = `Sk8rlog: ${postType}`
+
     useEffect( () => {
+        loadUser()
         verifyAccess(setHasAccess)
     }, [])
+
     useEffect( () => {
         hasAccess === false && navigate('/unauthorized')
     }, [hasAccess])
-
-    const [isReadyToDisplayContent, setIsReadyToDisplayContent] = useState(false)
-    const [posts, setPosts] = useState(null)
 
     useEffect( () => {
         setIsReadyToDisplayContent(false)
         getAllPostsByType(postType, setPosts, { isScoring: true, activeUser })
         setIsReadyToDisplayContent(true)
-    }, [postType])
+    }, [postType, activeUser])
 
+    useEffect( () => {
+        switch (filterState) {
+            case RECOMMENDED :
+            case POPULAR:
+                getAllPostsByType(postType, setPosts, { isScoring: true, activeUser })
+                break
 
-    const HEADER_TEXT = `Sk8rlog: ${postType}`
+            // TODO Implement locations
+            case NEAR_YOU :
+            case LATEST :
+                getAllPostsByType(postType, setPosts)
+                break
+
+            default :
+                console.error("Posts: invalid filer provided")
+                getAllPostsByType(postType, setPosts)
+        }
+    }, [filterState])
+
+    const handleFilterChange = (event) => {
+        event.preventDefault()
+        setFilterState(event.target.value)
+    }
 
     if (!isReadyToDisplayContent) return (<p>Loading posts...</p>)
 
@@ -47,17 +75,24 @@ const Posts = ({ postType }) => {
             <Sidebar />
             <section className='postsContent'>
                 <form className='postsHeader'>
-                    <select name='filter'>
-                        <option value='recommended'>Recommended</option>
-                        <option value='latest'>Latest Content</option>
-                        <option value='popular'>Popular</option>
-                        <option value='near'>Near You</option>
+                    <select name='filter' defaultValue={RECOMMENDED} onChange={handleFilterChange}>
+                        <option value={RECOMMENDED}>Recommended</option>
+                        <option value={LATEST}>Latest Content</option>
+                        <option value={POPULAR}>Popular</option>
+                        <option value={NEAR_YOU}>Near You</option>
                     </select>
                 </form>
 
                 <div className="posts">
-                {
+                { filterState !== POPULAR &&
                     posts?.map(post => {
+                        return (
+                            <PostCard key={post.postID} post={post} postType={postType} origin={POSTS[postType]} />
+                        )
+                    })
+                }
+                { filterState === POPULAR &&
+                    posts?.toSorted((a,b) => b.popularity - a.popularity).map(post => {
                         return (
                             <PostCard key={post.postID} post={post} postType={postType} origin={POSTS[postType]} />
                         )
