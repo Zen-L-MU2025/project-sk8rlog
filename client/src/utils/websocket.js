364001833e703e9c57b5c1c2d49b42485f9617fc
Zen Lambertus <zenl@meta.com>
2025-07-16 12:09:04 -0700

    more

--- client/src/utils/websocket.js
+++ client/src/utils/websocket.js
@@ -1,11 +1,24 @@
 import { io } from 'socket.io-client'
 const { VITE_SERVER_URL } = import.meta.env
 
-export const socket = () => {
+export const establishWebSocketConnection = () => {
     const socket = io(VITE_SERVER_URL)
     socket.on('connect', () => {
     })
+
     socket.on('disconnect', () => {
     })
-    return () => { socket.disconnect() }
+
+    socket.on('deliver notification', (content) => {
+        console.log(content)
+    })
+
+    periodicPing(socket)
+}
+
+const periodicPing = (socket) => {
+    const pingInterval = 5000
+    setInterval(() => {
+        socket.emit('request notification')
+    }, pingInterval)
 }
