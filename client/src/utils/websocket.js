a0f09aa791b46c7fc78f88732b953a34d1e4254d
Zen Lambertus <zenl@meta.com>
2025-07-16 12:23:46 -0700

    strict mode socket identifying

--- client/src/utils/websocket.js
+++ client/src/utils/websocket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client'
 const { VITE_SERVER_URL } = import.meta.env
 
 export const establishWebSocketConnection = () => {
+    const identifier = new Date().getMilliseconds()
     const socket = io(VITE_SERVER_URL)
     socket.on('connect', () => {
     })
@@ -10,14 +11,14 @@ export const establishWebSocketConnection = () => {
     })
 
     socket.on('deliver notification', (content) => {
-        console.log(content)
+        console.log(`Socket ${identifier}: ${content}`)
     })
 
     periodicPing(socket)
 }
 
 const periodicPing = (socket) => {
-    const pingInterval = 5000
+    const pingInterval = 10000
     setInterval(() => {
         socket.emit('request notification')
     }, pingInterval)
