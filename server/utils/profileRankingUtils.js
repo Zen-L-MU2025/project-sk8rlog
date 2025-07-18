689c13e9de444c080263782526b1848814f510eb
Zen Lambertus <zenl@meta.com>
2025-07-18 10:15:23 -0700

    flesh out profile ranking

--- server/utils/profileRankingUtils.js
+++ server/utils/profileRankingUtils.js
@@ -1,7 +1,13 @@
+const { PrismaClient } = require("../generated/prisma");
 const RECOMMENDED = "recommended";
 
+const prisma = new PrismaClient();
+
+// Rank suggestion candidates for some user and return candidates in descending order of score
 export const rankCandidates = async (user) => {
-    let candidates = [1, 2, 3, 4, 5]; // placeholder: prisma statement
+    let candidates = await prisma.user.findMany({
+        where: { userID: { in: user.followedUsers } },
+    });
 
     for (const candidate in candidates) {
         candidate["score"] = await evaluateCandidate(user, candidate, scorePosts);
@@ -12,6 +18,7 @@ export const rankCandidates = async (user) => {
     return candidates;
 };
 
+// Evaluate a suggestion candidate for a user based on their posts score, interests similarity, and mutual following
 export const evaluateCandidate = async (user, candidate, scorePosts) => {
     if (!user || !candidate) {
         console.log("evaluateCandidate: user or candidate is null");
@@ -23,11 +30,13 @@ export const evaluateCandidate = async (user, candidate, scorePosts) => {
         return -Infinity;
     }
 
-    const candidatePosts = candidate.posts; // prisma statement
+    // Rank posts using created recommendation algorithm
+    const candidatePosts = await prisma.post.findMany({ where: { authorID: candidate.userID } });
     const setCandidatePosts = (updatedPosts) => {
         candidatePosts = updatedPosts;
     };
 
+    // Average post scores an overall posts score
     scorePosts(candidatePosts, user, setCandidatePosts, RECOMMENDED);
     let totalScore = 0;
     for (const post of candidatePosts) {
@@ -35,22 +44,26 @@ export const evaluateCandidate = async (user, candidate, scorePosts) => {
     }
     const postOvr = totalScore / candidatePosts.length;
 
+    // Vectorize the users' frequency objects and perform a cosine similarity comparison
     const userFreq = user.userFrequency;
     const candidateFreq = candidate.userFrequency;
     // then vectorize
     // then pass insto cosine similarity function
-
     const similarityScore = 1738; // placeholder
+
+    // Convert the similarity score to a scalar factor
     const similarityFactor = similarityScore < 0 ? -1 + similarityScore : 1 + similarityScore;
 
+    // Find users' following overlap
     const userFollowing = user.followedUsers;
     const candidateFollowing = candidate.followedUsers;
     const followingOverlap = userFollowing.filter((user) => candidateFollowing.includes(user));
 
+    // Hold this overlap against all users they follow to create a mutual following factor
     const followingUnion = new Set(userFollowing.concat(candidateFollowing));
     const mutualFollowFrequency = followingOverlap.length / followingUnion.size;
 
+    // Finalize and return the candidate's score
     const candidateScore = postOvr * similarityFactor * (1 + mutualFollowFrequency);
-
     return candidateScore;
 };
