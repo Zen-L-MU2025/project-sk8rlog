f76cd5c911f21fea29e9cd508a0600b86114c753
Zen Lambertus <zenl@meta.com>
2025-07-24 20:21:51 -0700

    feature complete, pre-testing

--- server/utils/notifications/notifyUsersWithPost.js
+++ server/utils/notifications/notifyUsersWithPost.js
@@ -1,18 +1,25 @@
-import { POST_SCORE_THRESHOLD } from "../constants.js";
+import { POST_SCORE_THRESHOLD, POST_SUGGESTION } from "../constants.js";
 import { calculateSinglePostScoreWithUserObject } from "../postRecommendations/calculateSinglePostScore.js";
 import { PrismaClient } from "../../generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
 const notifyUsersWithPost = async (post, socketServer) => {
-    console.log("Let's ride");
     const users = await prisma.user.findMany({ where: { userID: { not: post.authorID } } });
 
     for (const user of users) {
         // Deep copy the provided post for synchronous processing
         const postCopy = JSON.parse(JSON.stringify(post));
         await calculateSinglePostScoreWithUserObject(postCopy, user);
-        console.log(`${user.username} X new post score: ${postCopy.score}, met threshold: ${postCopy.score >= POST_SCORE_THRESHOLD}`);
+
+        if (postCopy.score < POST_SCORE_THRESHOLD) continue;
+
+        socketServer.to(`user_${user.userID}`).emit(DELIVER_NOTIFICATION, {
+            recipient: [user.name || `@${user.username}`],
+            type: POST_SUGGESTION,
+            data: postCopy,
+            timestamp: new Date().toLocaleTimeString(),
+        });
     }
 };
 
