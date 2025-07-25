d882705613b62787bd6ca28a841e1e1958572e7a
Zen Lambertus <zenl@meta.com>
2025-07-25 01:18:40 -0700

    created proximity bias calculator and implemented in post and profile ranking

--- server/utils/postRecommendations/calculateSinglePostScore.js
+++ server/utils/postRecommendations/calculateSinglePostScore.js
@@ -3,6 +3,7 @@ import calculateBiasFactors from "./helpers/calculateBiasFactors.js";
 import filterTokens from "./helpers/filterTokens.js";
 import calculateTokenScore from "./helpers/calculateTokenScore.js";
 import getPostLength from "./helpers/getPostLength.js";
+import calculateProximityBias from "../calculateProximityBias.js";
 
 // Wrapper function for calculateSinglePostScore > takes in a user obj as argument to generate full set of params
 // Used in the post notifications flow
@@ -10,8 +11,16 @@ export const calculateSinglePostScoreWithUserObject = async (post, user) => {
     const userFrequency = user.user_Frequency;
     const { portionOfLikedPostsThatAreClips, avgLengthOfLikedPosts } = await calculateBiasFactors(user);
     const portionOfLikedPostsThatAreBlogs = 1 - portionOfLikedPostsThatAreClips;
+    const userLocation = user.location;
 
-    await calculateSinglePostScore(post, userFrequency, avgLengthOfLikedPosts, portionOfLikedPostsThatAreClips, portionOfLikedPostsThatAreBlogs);
+    await calculateSinglePostScore(
+        post,
+        userFrequency,
+        avgLengthOfLikedPosts,
+        portionOfLikedPostsThatAreClips,
+        portionOfLikedPostsThatAreBlogs,
+        userLocation
+    );
 };
 
 // Calculates a post's score and assigns it to the provided post object
@@ -20,7 +29,8 @@ const calculateSinglePostScore = async (
     userFrequency,
     avgLengthOfLikedPosts,
     portionOfLikedPostsThatAreClips,
-    portionOfLikedPostsThatAreBlogs
+    portionOfLikedPostsThatAreBlogs,
+    userLocation
 ) => {
     let rawPostScore = 0;
 
@@ -70,8 +80,12 @@ const calculateSinglePostScore = async (
     // Calculate bias factor as popularity amplified by type bias and inhibited by disparity from average post length
     const biasFactor = popularityScore * (1 + typeBias) * postLengthBias;
 
+    // Calculate proximity bias as constant factor added to overall score
+    const proximityBias = calculateProximityBias(userLocation, post.location);
+    console.log(`distance between ${userLocation} and ${post.location}, proximityBias is ${proximityBias}`);
+
     // Apply relative interest factor and bias factor to raw post score to get the final score
-    const finalScore = rawPostScore * relativeInterestFactor * biasFactor;
+    const finalScore = rawPostScore * relativeInterestFactor * biasFactor + proximityBias;
     post["score"] = finalScore;
     post["popularity"] = popularityScore;
 };
